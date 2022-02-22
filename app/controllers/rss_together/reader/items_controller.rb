99fd2d71e6b3c8119da7f24afecf35b08422e911
Jaryl Sim <jaryl.sim@me.com>
2022-02-22 21:20:21 +0800

    Initial work on reader item pagination.

--- app/controllers/rss_together/reader/items_controller.rb
+++ app/controllers/rss_together/reader/items_controller.rb
@@ -1,11 +1,15 @@
 module RssTogether
   module Reader
     class ItemsController < BaseController
+      ITEM_LIMIT = 10
+
       before_action :prepare_group
 
       def index
-        # TODO: retrieve only latest n items, pagination, etc
-        @items = @group.items.order(published_at: :desc)
+        paginate_with_cursor do |query|
+          @items = @group.items.order(published_at: :desc).limit(ITEM_LIMIT)
+          @items = @items.where("published_at < ?", query[:published_at]) if query[:published_at].present?
+        end
       end
 
       def show
@@ -16,6 +20,28 @@ module RssTogether
 
       def prepare_group
         @group = current_account.groups.find(params[:group_id])
+      end
+
+      def paginate_with_cursor
+        @current_cursor = params[:cursor] || ""
+
+        yield @current_cursor.blank? ? {} :deconstruct_cursor(@current_cursor)
+
+        if @items.empty?
+          @next_cursor = ""
+        else
+          @next_cursor = construct_cursor({
+            published_at: @items.last.published_at,
+          })
+        end
+      end
+
+      def construct_cursor(params)
+        Base64.encode64(params.to_json)
+      end
+
+      def deconstruct_cursor(cursor)
+        JSON.parse(Base64.decode64(cursor)).with_indifferent_access
       end
     end
   end

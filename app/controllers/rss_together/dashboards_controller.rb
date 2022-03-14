f33db29ac426908ee0ca5ee3aec44f2e56e0b575
Jaryl Sim <jaryl.sim@me.com>
2022-03-14 10:46:21 +0800

    Remove unused dashboards controller.

--- app/controllers/rss_together/dashboards_controller.rb
+++
@@ -1,9 +0,0 @@
-module RssTogether
-  class DashboardsController < ApplicationController
-    layout "application"
-
-    def show
-      @invitations = Invitation.where(email: current_account.email)
-    end
-  end
-end

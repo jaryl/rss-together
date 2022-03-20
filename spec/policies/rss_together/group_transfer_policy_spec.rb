a3d56b65d6513d620cfea5b385f260ebd6b9f455
Jaryl Sim <jaryl.sim@me.com>
2022-03-20 13:30:33 +0800

    Reduce noise from pending policy specs.

--- spec/policies/rss_together/group_transfer_policy_spec.rb
+++ spec/policies/rss_together/group_transfer_policy_spec.rb
@@ -6,24 +6,6 @@ module RssTogether
 
     subject { described_class }
 
-    permissions ".scope" do
-      pending "add some examples to (or delete) #{__FILE__}"
-    end
-
-    permissions :show? do
-      pending "add some examples to (or delete) #{__FILE__}"
-    end
-
-    permissions :create? do
-      pending "add some examples to (or delete) #{__FILE__}"
-    end
-
-    permissions :update? do
-      pending "add some examples to (or delete) #{__FILE__}"
-    end
-
-    permissions :destroy? do
-      pending "add some examples to (or delete) #{__FILE__}"
-    end
+    pending
   end
 end

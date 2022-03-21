e250d7d929691de21054fdaa84d45a28e7295097
Jaryl Sim <jaryl.sim@me.com>
2022-03-21 09:42:08 +0800

    Fix parsing issues with RSS feeds.

--- app/lib/rss_together/rss_interface.rb
+++ app/lib/rss_together/rss_interface.rb
@@ -11,12 +11,13 @@ module RssTogether
     end
 
     def feed
+      updated_at = DateTime.parse(document.at_xpath("//channel/lastBuildDate").text) rescue nil
       FeedElement.new({
         link: document.at_xpath("//channel/link").text,
         title: document.at_xpath("//channel/title").text,
         description: document.at_xpath("//channel/description").text,
         language: "",
-        updated_at: DateTime.parse(document.at_xpath("//channel/lastBuildDate").text),
+        updated_at: updated_at,
       })
     rescue StandardError => e
       raise RssDocumentParsingError, e.message
@@ -29,15 +30,16 @@ module RssTogether
         pub_date = DateTime.parse(item.at_xpath("pubDate").text)
         creators = item.xpath("creator").collect { |e| e.text.strip }
         authors = item.xpath("author/name").collect { |e| e.text.strip }
+        guid = item.at_xpath("guid").text rescue Digest::MD5.hexdigest(content_encoded.blank? ? description : content_encoded)
 
         ItemElement.new({
           title: item.at_xpath("title").text,
           content: content_encoded.blank? ? description : content_encoded,
           link: item.at_xpath("link").text,
           description: strip_tags([description, content_encoded].reject(&:blank?).first)&.strip&.truncate(140),
-          author: (creators + authors).reject(&:blank?).uniq,
+          author: (creators + authors).reject(&:blank?).uniq.join(", "),
           published_at: pub_date,
-          guid: item.at_xpath("guid").text,
+          guid: guid,
           updated_at: pub_date,
           categories: item.xpath("category").collect(&:text),
         })

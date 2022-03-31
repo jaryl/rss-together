b70be06c9bc981398c7b66b6da79d2258473e84b
Jaryl Sim <jaryl.sim@me.com>
2022-03-31 13:08:27 +0800

    Fix for dummy app engine configuration.

--- spec/dummy/config/initializers/rss_together.rb
+++ spec/dummy/config/initializers/rss_together.rb
@@ -1,13 +1,17 @@
-RssTogether.feed_processing_interval = 12.hours
+RssTogether.setup do |config|
+  config.feed_processing_interval = 12.hours
 
-RssTogether.items_are_unread_if_published_within = 30.days
-RssTogether.unread_system_markers_expire_after = 30.days
-RssTogether.unread_user_markers_expire_after = 45.days
+  config.items_are_unread_if_published_within = 30.days
+  config.unread_system_markers_expire_after = 30.days
+  config.unread_user_markers_expire_after = 45.days
 
-RssTogether.invitations_expire_after = 2.weeks
+  config.invitations_expire_after = 2.weeks
 
-RssTogether.group_transfers_expire_after = 48.hours
+  config.group_transfers_expire_after = 48.hours
 
-RssTogether.max_links_followed_to_resolve_url = 3
+  config.max_links_followed_to_resolve_url = 3
 
-RssTogether.user_agent = "RssTogether"
+  config.user_agent = "RssTogether"
+
+  config.error_reporter = ->(error) {}
+end

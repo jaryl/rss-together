53395139272b90a47eecedcec31f019650e5ad6f
Jaryl Sim <jaryl.sim@me.com>
2022-04-01 01:34:49 +0800

    Skip authorisation checks for rodauth controllers.

--- app/controllers/admin/rodauth_controller.rb
+++ app/controllers/admin/rodauth_controller.rb
@@ -2,4 +2,7 @@ class Admin::RodauthController < ApplicationController
   # used by Rodauth for rendering views, CSRF protection, and running any
   # registered action callbacks and rescue_from handlers
   layout "basic"
+
+  before_action :skip_authorization
+  before_action :skip_policy_scope
 end
--- app/controllers/rodauth_controller.rb
+++ app/controllers/rodauth_controller.rb
@@ -4,4 +4,7 @@ class RodauthController < ApplicationController
   layout "basic"
 
   skip_before_action :redirect_if_not_onboarded
+
+  before_action :skip_authorization
+  before_action :skip_policy_scope
 end

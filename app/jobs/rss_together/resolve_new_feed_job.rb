5ce460bddfe5e57aaa21d2f22e7f345f7d09f18e
Jaryl Sim <jaryl.sim@me.com>
2022-06-08 22:06:58 +0800

    Improve error reporting on jobs by including feed urls in context.

--- app/jobs/rss_together/resolve_new_feed_job.rb
+++ app/jobs/rss_together/resolve_new_feed_job.rb
@@ -5,21 +5,21 @@ module RssTogether
     attr_reader :subscription_request
 
     discard_on NoFeedAtTargetUrlError do |job, error|
-      job.fail_with_feedback(resource: job.subscription_request, error: error) do |feedback|
+      job.fail_with_feedback(resource: job.subscription_request, error: error, context: context) do |feedback|
         feedback.message = error.message
         job.subscription_request.update!(status: :failure)
       end
     end
 
     discard_on DocumentParsingError do |job, error|
-      job.fail_with_feedback(resource: job.subscription_request, error: error) do |feedback|
+      job.fail_with_feedback(resource: job.subscription_request, error: error, context: context) do |feedback|
         feedback.message = "There was a problem processing the content at #{job.subscription_request.target_url}"
         job.subscription_request.update!(status: :failure)
       end
     end
 
     discard_on Faraday::Error do |job, error|
-      job.fail_with_feedback(resource: job.subscription_request, error: error) do |feedback|
+      job.fail_with_feedback(resource: job.subscription_request, error: error, context: context) do |feedback|
         feedback.message = "Encountered a server error at #{job.subscription_request.target_url}"
         job.subscription_request.update!(status: :failure)
       end
@@ -66,6 +66,10 @@ module RssTogether
       resolved_url = URI(link).host.nil? ? URI.join(subscription_request.target_url, link) : link
       subscription_request.update!(target_url: resolved_url)
       ResolveNewFeedJob.perform_later(subscription_request, follows + 1)
+    end
+
+    def context
+      { feed_url: subscription_request.target_url }
     end
   end
 end
